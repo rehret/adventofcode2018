c6111703325c8464212c2f8ae650351f09478956
Randy Ehret <randyehret@gmail.com>
2018-12-01 18:43:54 -0600

    Use Set<number> instead of Array<number> for solution in day-01/puzzle-02
    This improves performance since checking for a number in an array is expensive.

--- src/day-01/puzzle-02/puzzle-02.ts
+++ src/day-01/puzzle-02/puzzle-02.ts
@@ -3,21 +3,21 @@ import { parse } from '../lib/parser';
 
 export function findDuplicateFrequency(input: string): number {
 	const frequencyPattern = split(input.trim()).map((str) => parse(str));
-	const previousFrequencies: number[] = [];
+	const previousFrequencies = new Set<number>();
 	let duplicateFrequency: number | null = null;
 
 	let frequencyDeltaIndex = 0;
 	let frequency = 0;
-	previousFrequencies.push(frequency);
+	previousFrequencies.add(frequency);
 
 	while (duplicateFrequency === null) {
 		const frequencyDelta = frequencyPattern[frequencyDeltaIndex];
 
 		frequency += frequencyDelta;
-		if (previousFrequencies.includes(frequency)) {
+		if (previousFrequencies.has(frequency)) {
 			duplicateFrequency = frequency;
 		} else {
-			previousFrequencies.push(frequency);
+			previousFrequencies.add(frequency);
 		}
 		frequencyDeltaIndex = (frequencyDeltaIndex + 1) % frequencyPattern.length;
 	}
