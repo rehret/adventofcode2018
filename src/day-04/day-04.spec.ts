ecf3255388d2fe1e0910e735417f637a579b7da0
Randy Ehret <randyehret@gmail.com>
2018-12-04 14:20:31 -0600

    Add unit tests for day-04/puzzle-01

--- src/day-04/day-04.spec.ts
+++ src/day-04/day-04.spec.ts
@@ -1,7 +1,220 @@
 import { assert } from 'chai';
+import { Event } from './lib/event';
 import { getTargetGuardAndMinute } from './puzzle-01';
+import { GuardEventType, GuardEvent } from './lib/guard-event';
+import { GuardDetail } from './lib/guard-detail';
+import { getGuardDetails } from './lib/guard-event-log-processor';
 
 describe('day-04', () => {
+	describe('lib', () => {
+		describe('Event', () => {
+			it('should assign properties from constructor parameters', () => {
+				// Arrange
+				const isoTimestamp = '2018-01-01 01:02:03';
+				const inputTimestamp = new Date(isoTimestamp);
+				const inputMessage = 'test event message';
+
+				// Act
+				const result = new Event(inputTimestamp, inputMessage);
+
+				// Assert
+				assert.deepEqual(result.timestamp, new Date(isoTimestamp));
+				assert.equal(result.message, inputMessage);
+			});
+		});
+
+		describe('GuardEvent', () => {
+			it('should assign properties from constructor parameters', () => {
+				// Arrange
+				const isoTimestamp = '2018-01-01 01:02:03';
+				const inputTimestamp = new Date(isoTimestamp);
+				const inputId = 7;
+				const inputEventType = GuardEventType.WakeUp;
+
+				// Act
+				const result = new GuardEvent(inputTimestamp, inputId, inputEventType);
+
+				// Assert
+				assert.deepEqual(result.timestamp, new Date(isoTimestamp));
+				assert.equal(result.guardId, inputId);
+				assert.equal(result.eventType, GuardEventType.WakeUp);
+			});
+		});
+
+		describe('GuardDetail', () => {
+			describe('constructor', () => {
+				it('should assign properties from constructor parameters', () => {
+					// Arrange
+					const inputId = 7;
+
+					// Act
+					const result = new GuardDetail(inputId);
+
+					// Assert
+					assert.equal(result.id, inputId);
+				});
+
+				it('should initialize minutesAsleep property', () => {
+					// Act
+					const result = new GuardDetail(1);
+
+					// Assert
+					assert.isDefined(result.minutesAsleep);
+					assert.isTrue(result.minutesAsleep instanceof Map);
+				});
+			});
+
+			describe('totalMinutesAsleep property', () => {
+				it('should sum the minutes asleep', () => {
+					// Arrange
+					const guard = new GuardDetail(1);
+					guard.minutesAsleep.set(1, 1);
+					guard.minutesAsleep.set(2, 10);
+					guard.minutesAsleep.set(3, 5);
+					guard.minutesAsleep.set(5, 20);
+
+					// Act
+					const totalMinutesAsleep = guard.totalMinutesAsleep;
+
+					// Assert
+					assert.equal(totalMinutesAsleep, 36);
+				});
+
+				it('should return 0 if the guard was never asleep', () => {
+					// Arrange
+					const guard = new GuardDetail(1);
+
+					// Act
+					const totalMinutesAsleep = guard.totalMinutesAsleep;
+
+					// Assert
+					assert.equal(totalMinutesAsleep, 0);
+				});
+			});
+		});
+
+		describe('getGuardDetails', () => {
+			it('should parse the guard details from the guard event log', () => {
+				// Arrange
+				const input = `
+					[2018-01-01 00:01] Guard #123 begins shift
+					[2018-01-01 00:02] falls asleep
+					[2018-01-01 00:03] wakes up
+					[2018-01-01 00:05] falls asleep
+					[2018-01-01 00:10] wakes up
+					[2018-01-02 00:01] Guard #123 begins shift
+					[2018-01-02 00:05] falls asleep
+					[2018-01-02 00:06] wakes up
+				`;
+
+				// Act
+				const guards = getGuardDetails(input);
+
+				// Assert
+				assert.isArray(guards);
+				assert.equal(guards.length, 1);
+				assert.equal(guards[0].id, 123);
+				assert.equal(guards[0].totalMinutesAsleep, 7);
+				assert.equal(guards[0].minutesAsleep.get(2), 1);
+				assert.equal(guards[0].minutesAsleep.get(5), 2);
+				assert.equal(guards[0].minutesAsleep.get(6), 1);
+				assert.equal(guards[0].minutesAsleep.get(7), 1);
+				assert.equal(guards[0].minutesAsleep.get(8), 1);
+				assert.equal(guards[0].minutesAsleep.get(9), 1);
+			});
+
+			it('should handle multiple guards', () => {
+				// Arrange
+				const input = `
+					[2018-01-01 00:01] Guard #123 begins shift
+					[2018-01-02 00:01] Guard #456 begins shift
+				`;
+
+				// Act
+				const guards = getGuardDetails(input);
+
+				// Assert
+				assert.equal(guards.length, 2);
+				assert.equal(guards[0].id, 123);
+				assert.equal(guards[1].id, 456);
+			});
+
+			it('should not set any minutes asleep if guard never slept', () => {
+				// Arrange
+				const input = `
+					[2018-01-01 00:01] Guard #1 begins shift
+				`;
+
+				// Act
+				const guards = getGuardDetails(input);
+
+				// Assert
+				assert.equal(guards[0].minutesAsleep.size, 0);
+			});
+
+			it('should sort events by timestamp', () => {
+				// Arrange
+				const input = `
+					[2018-01-02 00:01] Guard #2 begins shift
+					[2018-01-01 00:02] falls asleep
+					[2018-01-01 00:03] wakes up
+					[2018-01-01 00:01] Guard #1 begins shift
+				`;
+
+				// Act
+				const guards = getGuardDetails(input);
+
+				// Assert
+				assert.equal(guards.length, 2);
+				const guard1 = guards.find((g) => g.id === 1) as GuardDetail;
+				const guard2 = guards.find((g) => g.id === 2) as GuardDetail;
+
+				assert.equal(guard1.minutesAsleep.size, 1);
+				assert.equal(guard1.minutesAsleep.get(2), 1);
+				assert.equal(guard1.totalMinutesAsleep, 1);
+
+				assert.equal(guard2.minutesAsleep.size, 0);
+				assert.equal(guard2.totalMinutesAsleep, 0);
+			});
+		});
+
+		it('should throw an error if the input is in an invalid format', () => {
+			// Arrange
+			const input = `
+				[2018-01-01] Guard #1 begins shift
+			`;
+
+			// Act & Assert
+			assert.throws(() => getGuardDetails(input));
+		});
+
+		it('should throw an error if the event is an unrecognized event', () => {
+			// Arrange
+			const input = `
+				[2018-01-01 00:01] Guard #1 ends shift
+			`;
+
+			// Act & Assert
+			assert.throws(() => getGuardDetails(input));
+		});
+
+		it('should ignore events that occur before the first shift start', () => {
+			// Arrange
+			const input = `
+				[2018-01-01 00:01] falls asleep
+				[2018-01-01 00:02] wakes up
+				[2018-01-01 00:03] Guard #1 begins shift
+			`;
+
+			// Act
+			const guards = getGuardDetails(input);
+
+			// Assert
+			assert.equal(guards.length, 1);
+			assert.equal(guards[0].id, 1);
+		});
+	});
+
 	describe('puzzle-01', () => {
 		it('should return 240 for the example problem input', () => {
 			// Arrange
@@ -30,6 +243,30 @@ describe('day-04', () => {
 
 			// Assert
 			assert.equal(result, 240);
+		});
+
+		it('should find guard with most sleeping minutes', () => {
+			// Arrange
+			const input = `
+				[2018-01-01 00:01] Guard #1 begins shift
+				[2018-01-01 00:02] falls asleep
+				[2018-01-01 00:03] wakes up
+				[2018-01-01 00:04] Guard #2 begins shift
+				[2018-01-01 00:05] falls asleep
+				[2018-01-01 00:07] wakes up
+				[2018-01-01 00:08] Guard #3 begins shift
+				[2018-01-01 00:09] falls asleep
+				[2018-01-01 00:11] wakes up
+				[2018-01-01 00:08] Guard #3 begins shift
+				[2018-01-01 00:09] falls asleep
+				[2018-01-01 00:10] wakes up
+			`;
+
+			// Act
+			const result = getTargetGuardAndMinute(input);
+
+			// Assert
+			assert.equal(result, 27);
 		});
 	});
 });
